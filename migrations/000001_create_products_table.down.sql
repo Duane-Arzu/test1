95504ad4d310fcc9c369e9d7d65fc2cbfa6cb685
Duane M.  Arzu <2020152202@ub.edu.bz>
2024-11-01 18:20:22 -0600

    test1 trial and error 10

--- migrations/000001_create_products_table.down.sql
+++ migrations/000001_create_products_table.down.sql
@@ -1,5 +1,9 @@
--- Filename: migrations/000001_create_products_table.down.sql
+-- Drop the trigger first as it depends on the function and table
 DROP TRIGGER IF EXISTS update_product_rating ON reviews;
+
+-- Drop the function next
 DROP FUNCTION IF EXISTS automatic_average_rating();
+
+-- Drop the tables, starting with the one that references another table
 DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS products;
--- migrations/000001_create_products_table.up.sql
+++ migrations/000001_create_products_table.up.sql
@@ -1,21 +1,11 @@
--- Filename: migrations/000001_create_products_table.up.sql
-
--- CREATE TABLE IF NOT EXISTS comments (
---     id bigserial PRIMARY KEY,
---     created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
---     content text NOT NULL,
---     author text NOT NULL,
---     version integer NOT NULL DEFAULT 1
--- );
-
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE products (
     product_id bigserial PRIMARY KEY,
     name text NOT NULL,
     description text NOT NULL,
     category text NOT NULL,
     image_url text NOT NULL,
     price text NOT NULL,
-    average_rating DECIMAL(3, 2) DEFAULT 0.00, 
+    avg_rating DECIMAL(3, 2) DEFAULT 0.00, -- Average rating from reviews
     created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
     version integer NOT NULL DEFAULT 1
 );
@@ -25,7 +15,7 @@ CREATE TABLE reviews (
     product_id INT REFERENCES products(product_id) ON DELETE CASCADE,
     author VARCHAR(255),
     rating FLOAT CHECK (rating BETWEEN 1 AND 5),
-    review_text text NOT NULL,
+    comment text NOT NULL,
     helpful_count INT DEFAULT 0,
     created_at timestamp(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
     version integer NOT NULL DEFAULT 1
@@ -34,8 +24,9 @@ CREATE TABLE reviews (
 CREATE OR REPLACE FUNCTION automatic_average_rating()
 RETURNS TRIGGER AS $$
 BEGIN
+    -- Update the average rating of the product associated with the new review
     UPDATE products
-    SET average_rating = (
+    SET avg_rating = (
         SELECT ROUND(CAST(AVG(rating) AS NUMERIC), 2)
         FROM reviews
         WHERE reviews.product_id = NEW.product_id
@@ -50,4 +41,3 @@ CREATE OR REPLACE TRIGGER update_product_rating
 AFTER INSERT OR UPDATE OR DELETE ON reviews
 FOR EACH ROW
 EXECUTE FUNCTION automatic_average_rating();
-    
